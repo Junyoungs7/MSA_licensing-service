a8ed3e917149bedf55e39a02bd2e584f8bb11c08
김준영 <gimjun-yeong@Kim-JunYoung-ui-MacBookPro.local>
2022-10-13 14:14:19 +0900

    Initial commit

---
+++ src/main/java/com/optimagrowth/license/LicensingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.optimagrowth.license;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicensingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicensingServiceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/optimagrowth/license/service/LicenseService.java
@@ -0,0 +1,21 @@
+package com.optimagrowth.license.service;
+
+import com.optimagrowth.license.model.License;
+import org.springframework.stereotype.Service;
+
+import java.util.Random;
+
+@Service
+public class LicenseService {
+
+    public License getLicense(String licenseId, String organizationId){
+        License license = new License();
+        license.setId(new Random().nextInt(1000));
+        license.setLicenseId(licenseId);
+        license.setOrganizationId(organizationId);
+        license.setDescription("Software product");
+        license.setProductName("Ostock");
+        license.setLicenseType("full");
+        return license;
+    }
+}
